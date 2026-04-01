8533c6fdf9c0a7bfdea17cf07d0b5efba8e791b9
System Administrator <root@Website-Agent.local>
2026-04-01 10:23:43 -0400

    Add Meta Pixel 866028304580544

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,15 @@ export default function RootLayout({
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MN3BDCWH');`,
           }}
         />
+        {/* Meta Pixel */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','866028304580544');fbq('track','PageView');`,
+          }}
+        />
+        <noscript>
+          <img height="1" width="1" style={{ display: "none" }} src="https://www.facebook.com/tr?id=866028304580544&ev=PageView&noscript=1" alt="" />
+        </noscript>
         {/* MegaTag tracking */}
         <script
           dangerouslySetInnerHTML={{
